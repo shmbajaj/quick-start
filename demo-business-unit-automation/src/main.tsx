73bc706e52cc2e67d17a3c9dedddaaa9e976f788
Shubham Bajaj <shmbajaj32@gmail.com>
2023-10-25 11:35:00 +0000

    using react-router

--- demo-business-unit-automation/src/main.tsx
+++ demo-business-unit-automation/src/main.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { ThemeProvider } from './components/theme-provider.tsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "./components/theme-provider.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Meetings from "./routes/meetings.tsx";
+import Payments from "./routes/payments.tsx";
+import Root from "./routes/root.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { element: <Meetings />, index: true },
+      { path: "meetings", element: <Meetings />, index: true },
+      { path: "payments", element: <Payments /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
 );
