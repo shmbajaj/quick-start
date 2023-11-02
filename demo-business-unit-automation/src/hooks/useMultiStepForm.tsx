03dc7ec66bbe8341f29feac792516dcf491fe86c
Shubham Bajaj <shmbajaj32@gmail.com>
2023-11-02 11:04:44 +0000

    headless hook for controlling wizard

---
+++ demo-business-unit-automation/src/hooks/useMultiStepForm.tsx
@@ -0,0 +1,40 @@
+import { ReactElement, useState } from "react";
+
+type MultiStepFormProps = ReactElement[];
+
+export default function useMultiStepForm(steps: MultiStepFormProps) {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+  const step = steps[currentStepIndex];
+  const totalSteps = steps.length;
+  const isFirstStep = currentStepIndex === 0;
+  const isLastStep = currentStepIndex === steps.length - 1;
+
+  function next() {
+    const nextStepIndex =
+      currentStepIndex >= steps.length - 1
+        ? currentStepIndex
+        : currentStepIndex + 1;
+    setCurrentStepIndex(nextStepIndex);
+  }
+
+  function back() {
+    const prevStepIndex =
+      currentStepIndex <= 0 ? currentStepIndex : currentStepIndex - 1;
+    setCurrentStepIndex(prevStepIndex);
+  }
+
+  function goto(index: number) {
+    setCurrentStepIndex(index);
+  }
+
+  return {
+    step,
+    totalSteps,
+    isFirstStep,
+    isLastStep,
+    currentStepIndex,
+    next,
+    back,
+    goto,
+  };
+}
