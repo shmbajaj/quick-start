16faa7baf83068f179cff2ae44a8d558b88b23a0
Shubham Bajaj <shmbajaj32@gmail.com>
2023-11-02 07:00:19 +0000

    imperative: created design form

--- demo-business-unit-automation/src/components/forms/design-form.tsx
+++ demo-business-unit-automation/src/components/forms/design-form.tsx
@@ -1,7 +1,18 @@
 import * as z from "zod";
-import { Form, FormField } from "../ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { FormFieldDefinitions } from "@/types";
+import { Input } from "../ui/input";
+import FormWrapper from "./form-wrapper";
 
 const DesignFormSchema = z.object({
   jobID: z.string(),
@@ -16,13 +27,63 @@ const DesignFormSchema = z.object({
 type DesignFormInput = z.infer<typeof DesignFormSchema>;
 
 const designFormFieldDefinitions: FormFieldDefinitions<DesignFormInput> = [
-    {
-        id: "client", 
-        name: "client",
-        label: "Client Name",
-        placeholder: ""
-    }
-]
+  {
+    id: "jobID",
+    name: "jobID",
+    label: "Job ID",
+    placeholder: "Enter job ID here...",
+    description: "This is the unique job ID.",
+    type: "text",
+  },
+  {
+    id: "model",
+    name: "model",
+    label: "Model",
+    placeholder: "Enter model here...",
+    description: "This is the model name.",
+    type: "text",
+  },
+  {
+    id: "client",
+    name: "client",
+    label: "Client Name",
+    placeholder: "Enter client name here...",
+    description: "This is your client name.",
+    type: "text",
+  },
+  {
+    id: "product",
+    name: "product",
+    label: "Product",
+    placeholder: "Enter product name here...",
+    description: "This is the product name.",
+    type: "text",
+  },
+  {
+    id: "siteAddress",
+    name: "siteAddress",
+    label: "Site Address",
+    placeholder: "Enter site address here...",
+    description: "This is the site address.",
+    type: "text",
+  },
+  {
+    id: "contactPerson",
+    name: "contactPerson",
+    label: "Contact Person",
+    placeholder: "Enter contact person here...",
+    description: "This is your contact person name.",
+    type: "text",
+  },
+  {
+    id: "payment",
+    name: "payment",
+    label: "Payment",
+    placeholder: "Enter payment details here...",
+    description: "This is the payment information.",
+    type: "text",
+  },
+];
 
 export default function DesignForm() {
   const form = useForm<DesignFormInput>({
@@ -39,13 +100,40 @@ export default function DesignForm() {
   });
 
   return (
-    <Form {...form}>
-      <form
-        className="space-y-8"
-        onSubmit={form.handleSubmit((designFormData) =>
-          console.log({ designFormData })
-        )}
-      ></form>
-    </Form>
+    <FormWrapper
+      title="Design Form"
+      description="Manage your account settings and set e-mail preferences."
+    >
+      <Form {...form}>
+        <form
+          className="space-y-8"
+          onSubmit={form.handleSubmit((designFormData) =>
+            console.log({ designFormData })
+          )}
+        >
+          {designFormFieldDefinitions.map((formField) => (
+            <FormField
+              key={formField.id}
+              name={formField.name}
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>{formField.label}</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder={formField.placeholder}
+                      type={formField.type}
+                    />
+                  </FormControl>
+                  <FormDescription>{formField.description}</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ))}
+        </form>
+      </Form>
+    </FormWrapper>
   );
 }
